a9b7f344380be21399729140d1c55225dcc128db
PAOLAJIMENEZ <apjimenez41@misena.edu.co>
2017-06-15 10:42:09 -0500

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Documento sin t&iacute;tulo</title>
+<style type="text/css">
+body{
+background: linear-gradient(to right, rgba(242,246,248,1) 0%, rgba(216,225,231,1) 0%, rgba(120,122,124,1) 100%);
+}
+</style>
+</head>
+
+<body>
+<table width="100%" height="948" align="center" >
+  <tr>
+    <td width="1280" height="222">
+	<?php
+	include('Banner.php');
+	?>	</td>
+  </tr>
+  <tr>
+    <td width="1280" height="62">
+	<?php
+	include('MenuOriginal2.php');
+	?>	</td>
+  </tr>
+  <tr>
+    <td width="1280" height="454">
+	<?php
+	include('Slider.html');
+	?>	</td>
+  </tr>
+  <tr>
+    <td width="1280" height="197">
+	<?php
+	include ('Footer.php');
+	?>
+	
+	</td>
+  </tr>
+</table>
+</body>
+</html>
